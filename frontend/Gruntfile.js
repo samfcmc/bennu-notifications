111118a232c3c7672b41d16eb825e16029086767
Samuel Coelho <samuelfcmc@gmail.com>
2015-06-29 17:00:12 +0100

    Added bower task to frontend module

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -1,5 +1,15 @@
 module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-bower-task');
+
     grunt.initConfig({
+        bower: {
+            install: {
+                options: {
+                    targetDir: 'src/main/webapp/bower_components',
+                    cleanBowerDir: true
+                }
+            }
+        }
     });
-    grunt.registerTask('default', []);
+    grunt.registerTask('default', ['bower:install']);
 };
