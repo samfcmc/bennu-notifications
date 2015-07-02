e27383246bad8af5dbf5b8d9304af8cd22cca372
Samuel Coelho <samuelfcmc@gmail.com>
2015-07-02 17:30:16 +0100

    Added stub for the success test case

--- test-backend/src/test/java/org/fenixedu/bennu/notifications/test/backend/api/GetApplicationsTest.java
+++ test-backend/src/test/java/org/fenixedu/bennu/notifications/test/backend/api/GetApplicationsTest.java
@@ -1,6 +1,13 @@
 package org.fenixedu.bennu.notifications.test.backend.api;
 
+import org.junit.Test;
+
 public class GetApplicationsTest extends AbstractAPITest {
     //TODO: Test case
+
+    @Test
+    public void success() {
+
+    }
 
 }
--- test-backend/src/test/java/org/fenixedu/bennu/notifications/test/backend/api/RegisterApplicationTest.java
+++ test-backend/src/test/java/org/fenixedu/bennu/notifications/test/backend/api/RegisterApplicationTest.java
@@ -1,6 +1,13 @@
 package org.fenixedu.bennu.notifications.test.backend.api;
 
+import org.junit.Test;
+
 public class RegisterApplicationTest extends AbstractAPITest {
     // TODO: Test cases
+
+    @Test
+    public void success() {
+
+    }
 
 }
