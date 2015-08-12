2dbec77c289e1f781fb73593f032a9f08234adc6
Samuel Coelho <samuelfcmc@gmail.com>
2015-08-12 16:55:08 +0100

    Removed unused frontend dependency

--- frontend/src/main/webapp/scripts/vendors.js
+++ frontend/src/main/webapp/scripts/vendors.js
@@ -3,7 +3,6 @@
 (function(window, require) {
   window.jQuery = require('jquery');
   require('bootstrap');
-  require('angular');
   window.React = require('react');
   window.ReactRouter = require('react-router');
   window.ReactBootstrap = require('react-bootstrap');
